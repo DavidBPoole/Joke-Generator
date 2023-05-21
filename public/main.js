ad04d3c1605fd1784cc02ce35c4f779a37d2a4de
David Poole <dbpoole7@gmail.com>
2023-05-21 18:08:14 -0500

    cleaned up structure making things easier to read and added notes to clarify what each section accomplishes

--- public/main.js
+++ public/main.js
@@ -6,8 +6,10 @@ import renderToDom from '../utils/renderFunction';
 // const joke = getRequest();
 // console.warn(joke);
 
+// Array to push new joke object into:
 const joke = [];
 
+// HTML DOM render structure to supply containers for information to be pushed to.
 const renderHtmlStructure = () => {
   const domString = `
   <h1 id="jokeTitle">Joke Generator</h1>
@@ -19,24 +21,28 @@ const renderHtmlStructure = () => {
   renderToDom('#app', domString);
 };
 
+// Render for first button:
 const renderJoke = () => {
   const domString = `
   <button "type="button" id="getJoke" class="btn btn-dark">Get Joke</button>`;
   renderToDom('#jokeBtnContainer', domString);
 };
 
+// Render for second button:
 const punchline = () => {
   const domString = `
   <button type="button" id="getJoke2" class="btn btn-dark">Get Punchline</button>`;
   renderToDom('#jokeBtnContainer', domString);
 };
 
+// Render for third button:
 const returnNewJoke = () => {
   const domString = `
   <button type="button" id="getJoke3" class="btn btn-dark">Get New Joke</button>`;
   renderToDom('#jokeBtnContainer', domString);
 };
 
+// Function to render joke setup (1st part of joke) and to also push the JSON object with the new joke upon the third button case based on the switch case events within the event lsiteners.
 const renderJokeText = (Obj) => {
   // console.warn(Obj);
   joke.push(Obj);
@@ -46,6 +52,7 @@ const renderJokeText = (Obj) => {
   punchline();
 };
 
+// Function to render the joke delivery and the 2nd button text before returning to the 1st button for a new joke.
 const renderPunchline = () => {
   const domString = `
   <h2>${joke[0].delivery}</h2>`;
@@ -54,6 +61,37 @@ const renderPunchline = () => {
   returnNewJoke();
 };
 
+// Event Listeners
+const events = () => {
+  document.querySelector('#jokeBtnContainer').addEventListener('click', (e) => {
+    e.preventDefault();
+    switch (e.target.id) {
+      case 'getJoke':
+        getRequest().then((value) => renderJokeText(value));
+        break;
+      case 'getJoke2':
+        renderPunchline();
+        break;
+      case 'getJoke3':
+        getRequest().then((value) => renderJokeText(value));
+        break;
+      default:
+        break;
+    }
+  });
+};
+
+// startApp / Initialize function controlling sequence of renders to DOM.
+const init = () => {
+  renderHtmlStructure();
+  renderJoke();
+  events();
+};
+init();
+
+// Below contains code that works, but became unnecessary once the event listener issue of being unable to "read properties of null" was resolved.
+
+// fetchjoke will display jokes to the DOM, however does not control the sequence.
 // const fetchJoke = () => {
 //   getRequest().then((item) => {
 //     renderToDom('#jokeContainer', item.setup);
@@ -61,6 +99,7 @@ const renderPunchline = () => {
 // };
 // fetchJoke();
 
+// Below eventListeners do not work because they need to be contoled by logic that will allow only the first to be rendered and the remaining sequentially so they do not throw errors since they are all competing for the same div ID to render, which will cause them to return NULL.
 // const getJokeEvent = () => {
 //   document.querySelector('#getJoke').addEventListener('click', () => {
 //     getJokeEvent.innerHTML = 'Button clicked';
@@ -79,35 +118,7 @@ const renderPunchline = () => {
 //   });
 // };
 
-const init = () => {
-  renderHtmlStructure();
-  renderJoke();
-  // getRequest();
-  // fetchJoke();
-  // punchline();
-  // returnNewJoke();
-  // getJokeEvent();
-  // getPunchlinEvent();
-  // getNewJoke();
-};
-init();
-
-document.querySelector('#jokeBtnContainer').addEventListener('click', (e) => {
-  e.preventDefault();
-  switch (e.target.id) {
-    case 'getJoke':
-      getRequest().then((value) => renderJokeText(value));
-      break;
-    case 'getJoke2':
-      renderPunchline();
-      break;
-    case 'getJoke3':
-      getRequest().then((value) => renderJokeText(value));
-      break;
-    default:
-      break;
-  }
-});
+// The order of functions being called within the startApp or init function matter since JS reads from top down. This is why the final event listener for the button above was read over the correct first button. Logic was needed to control which case would run first and then stop reading the remaining button texts.
 
 // TEST AREA BELOW ONLY ***************
 
@@ -126,3 +137,12 @@ document.querySelector('#jokeBtnContainer').addEventListener('click', (e) => {
 //   });
 // };
 // events();
+
+// init function call backs no longer needed due to event listener solution:
+// getRequest();
+// fetchJoke();
+// punchline();
+// returnNewJoke();
+// getJokeEvent();
+// getPunchlinEvent();
+// getNewJoke();
