38e2f74a2cf5a062a6ef339ad4453544c33ed56c
David Poole <dbpoole7@gmail.com>
2023-05-20 20:27:41 -0500

    initial setup and code submitted; partially working

--- public/main.js
+++ public/main.js
@@ -1,25 +1,89 @@
-// USE WITH FIREBASE AUTH
-// import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+import getRequest from '../api/promises';
+import renderToDom from '../utils/renderFunction';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
+const joke = getRequest();
+console.warn(joke);
+
+const renderHtmlStructure = () => {
+  const domString = `
+  <h1 id="jokeTitle">Joke Generator</h1>
+  <div id="jokeContainer"></div>
+  <div id="jokeBtnContainer"></div>
   `;
-  console.warn('YOU ARE UP AND RUNNING!');
-
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
+  renderToDom('#app', domString);
 };
 
+const renderJoke = () => {
+  const domString = `
+  <button "type="button" id="getJoke1" class="btn btn-dark">Get Joke</button>`;
+  renderToDom('#jokeBtnContainer', domString);
+};
+
+const punchline = () => {
+  const domString = `
+  <button type="button" id="getJoke2" class="btn btn-dark">Get Punchline</button>`;
+  renderToDom('#jokeBtnContainer', domString);
+};
+
+const returnNewJoke = () => {
+  const domString = `
+  <button type="button" id="getJoke3" class="btn btn-dark">Get New Joke</button>`;
+  renderToDom('#jokeBtnContainer', domString);
+};
+
+const fetchJoke = () => {
+  getRequest().then((item) => {
+    renderToDom('#jokeContainer', item.setup);
+  });
+};
+
+const getJokeEvent = () => {
+  document.querySelector('#getJoke1').addEventListener('click', () => {
+    getRequest().then(console.warn);
+  });
+};
+
+const getPunchlinEvent = () => {
+  document.querySelector('#getJoke2').addEventListener('click', () => {
+    getRequest().then(console.warn);
+  });
+};
+
+const getNewJoke = () => {
+  document.querySelector('#getJoke3').addEventListener('click', () => {
+    getRequest().then(console.warn);
+  });
+};
+
+const init = () => {
+  renderHtmlStructure();
+  getRequest();
+  renderJoke();
+  fetchJoke();
+  punchline();
+  returnNewJoke();
+  getJokeEvent();
+  getPunchlinEvent();
+  getNewJoke();
+};
 init();
+
+// TEST AREA BELOW ONLY ***************
+
+// const events = () => {
+//   document.querySelector(renderJoke).addEventListener('click', () => {
+//     getRequest().then(console.warn);
+//   });
+//   document.querySelector(punchline).addEventListener('click', () => {
+//     getRequest().then(console.warn);
+//   });
+//   document.querySelector(returnNewJoke).addEventListener('click', () => {
+//     getRequest().then(console.warn);
+//   });
+//   document.querySelector(fetchJoke).addEventListener('click', () => {
+//     fetchJoke().then(console.warn);
+//   });
+// };
+// events();
